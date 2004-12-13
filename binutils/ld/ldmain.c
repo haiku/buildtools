4616c259410b10138a62abbabe3dc8caa79fab06
Oliver Tappe <zooey@hirschkaefer.de>
2004-12-13 17:30:14 +0000

    - we no longer hard-patch ld to not use relocation combination (combreloc),   as the default (using it) seems to generate slightly smaller results and   not break anything.
    git-svn-id: file:///srv/svn/repos/haiku/trunk/buildtools@10421 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- binutils/ld/ldmain.c
+++ binutils/ld/ldmain.c
@@ -301,8 +301,7 @@ main (int argc, char **argv)
   link_info.notice_all = FALSE;
   link_info.nocopyreloc = FALSE;
   link_info.new_dtags = FALSE;
-  /* disable combreloc by default for BeOS: */
-  link_info.combreloc = FALSE;
+  link_info.combreloc = TRUE;
   link_info.eh_frame_hdr = FALSE;
   link_info.strip_discarded = TRUE;
   link_info.strip = strip_none;
