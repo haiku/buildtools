dab660cbe39096d7c0f82661b890e734cc7b2173
Ingo Weinhold <ingo_weinhold@gmx.de>
2005-12-10 20:07:57 +0000

    * The .plt section for PPC is of type SHT_NOBITS (not SHT_PROGBITS as   for x86), so it should be placed near the .bss sections. This fixes   linkage problems -- the linker had to split the text segment after the   .plt section, which caused its pre-computed program header count to be   wrong. * I also added the stuff concerning the .fixup and .got{1,2} sections I   found in the generic ELF PPC script. It shouldn't harm at least.
    git-svn-id: file:///srv/svn/repos/haiku/buildtools/trunk@15474 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- binutils/ld/emulparams/elf_ppc_haiku.sh
+++ binutils/ld/emulparams/elf_ppc_haiku.sh
@@ -8,3 +8,13 @@ MACHINE=
 NOP=0x60000000
 TEMPLATE_NAME=elf32
 GENERATE_SHLIB_SCRIPT=yes
+BSS_PLT=
+OTHER_RELRO_SECTIONS="
+  .fixup        ${RELOCATING-0} : { *(.fixup) }
+  .got1         ${RELOCATING-0} : { *(.got1) }
+  .got2         ${RELOCATING-0} : { *(.got2) }
+"
+OTHER_GOT_RELOC_SECTIONS="
+  .rela.got1         ${RELOCATING-0} : { *(.rela.got1) }
+  .rela.got2         ${RELOCATING-0} : { *(.rela.got2) }
+"
