61e8e0f75cd14f6e5d0af6ed32c9943e2a775cb1
Oliver Tappe <zooey@hirschkaefer.de>
2006-08-13 14:30:12 +0000

    * added haiku-<date> pattern to version such that it is (from now on)   possible to differentiate all the jam-versions out there...
    git-svn-id: file:///srv/svn/repos/haiku/buildtools/trunk@18505 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- jam/patchlevel.h
+++ jam/patchlevel.h
@@ -1,5 +1,5 @@
 /* Keep JAMVERSYM in sync with VERSION. */
 /* It can be accessed as $(JAMVERSION) in the Jamfile. */
 
-#define VERSION "2.5rc3"
+#define VERSION "2.5-haiku-20060813"
 #define JAMVERSYM "JAMVERSION=2.5"
