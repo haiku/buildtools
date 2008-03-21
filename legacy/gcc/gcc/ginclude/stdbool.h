eb680fdd74280e46a9dcd9124c1b83141ecde332
Ingo Weinhold <ingo_weinhold@gmx.de>
2008-03-21 23:40:50 +0000

    Ugh, didn't notice that committing this change failed days ago. Wasn't a problem, since only the copies of gcc headers in the haiku module are used.
    BeOS/Haiku requires sizeof(bool) to be 1.
    
    
    git-svn-id: file:///srv/svn/repos/haiku/buildtools/trunk@24512 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- legacy/gcc/gcc/ginclude/stdbool.h
+++ legacy/gcc/gcc/ginclude/stdbool.h
@@ -2,6 +2,13 @@
 #ifndef __STDBOOL_H__
 #define __STDBOOL_H__	1
 
+#ifdef __BEOS__
+	typedef unsigned char _Bool;
+	#define bool _Bool
+	#define false 0
+	#define true 1
+#else
+
 /* The type `bool' must promote to `int' or `unsigned int'.  The constants
    `true' and `false' must have the value 0 and 1 respectively.  */
 typedef enum
@@ -13,6 +20,8 @@ typedef enum
 /* The names `true' and `false' must also be made available as macros.  */
 #define false	false
 #define true	true
+
+#endif
 
 /* Signal that all the definitions are present.  */
 #define __bool_true_false_are_defined	1
