257de429327943bde32512cd7447c32a729ce86c
Jonas Sundström <jonas@kirilla.com>
2009-05-02 18:20:14 +0000

    Tweaks. Removing superflous config. Cleaner and closer to defaults, hopefully.
    git-svn-id: file:///srv/svn/repos/haiku/buildtools/trunk@30580 a95241bf-73f2-0310-859d-f6bbb57e9c96

--- gcc/gcc/config/mips/haiku.h
+++ gcc/gcc/config/mips/haiku.h
@@ -20,7 +20,8 @@ the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
 
-#define TARGET_VERSION fprintf (stderr, " (mips Haiku/ELF)");
+#undef TARGET_VERSION
+#define TARGET_VERSION fprintf (stderr, " (MIPSEL Haiku/ELF)");
 
 #undef SIZE_TYPE
 #define SIZE_TYPE "long unsigned int"
@@ -34,15 +35,12 @@ Boston, MA 02111-1307, USA.  */
 #undef WCHAR_TYPE_SIZE
 #define WCHAR_TYPE_SIZE 16
 
-/* If we don't set MASK_ABICALLS, we can't default to PIC.  */
-#undef TARGET_DEFAULT
-#define TARGET_DEFAULT MASK_ABICALLS
-
 #define TARGET_OS_CPP_BUILTINS()					\
   do									\
     {									\
 	builtin_define ("__HAIKU__");					\
 	builtin_define ("__MIPS__");					\
+	builtin_define ("__MIPSEL__");					\
 	builtin_define ("_MIPSEL_");					\
 	builtin_define ("__stdcall=__attribute__((__stdcall__))");	\
 	builtin_define ("__cdecl=__attribute__((__cdecl__))");		\
@@ -61,76 +59,6 @@ Boston, MA 02111-1307, USA.  */
     }									\
   while (0)
 
-/* Provide a LINK_SPEC appropriate for Haiku.  Here we provide support
-   for the special GCC options -static and -shared, which allow us to
-   link things in one of these three modes by applying the appropriate
-   combinations of options at link-time.  */
-
-/* If ELF is the default format, we should not use /lib/elf.  */
-
 #undef	LINK_SPEC
 #define LINK_SPEC "%{!o*:-o %b} -m elf_mipsel_haiku -shared -Bsymbolic %{nostart:-e 0}"
-
-/* A C statement (sans semicolon) to output to the stdio stream
-   FILE the assembler definition of uninitialized global DECL named
-   NAME whose size is SIZE bytes and alignment is ALIGN bytes.
-   Try to use asm_output_aligned_bss to implement this macro.  */
-
-#define BSS_SECTION_ASM_OP	"\t.section\t.bss"
-
-#define ASM_OUTPUT_ALIGNED_BSS(FILE, DECL, NAME, SIZE, ALIGN) \
-  asm_output_aligned_bss (FILE, DECL, NAME, SIZE, ALIGN)
-
-#undef ASM_DECLARE_OBJECT_NAME
-#define ASM_DECLARE_OBJECT_NAME mips_declare_object_name
-
-#undef FUNCTION_NAME_ALREADY_DECLARED
-#define FUNCTION_NAME_ALREADY_DECLARED 1
-
-/* The glibc _mcount stub will save $v0 for us.  Don't mess with saving
-   it, since ASM_OUTPUT_REG_PUSH/ASM_OUTPUT_REG_POP do not work in the
-   presence of $gp-relative calls.  */
-#undef ASM_OUTPUT_REG_PUSH
-#undef ASM_OUTPUT_REG_POP
-
-/* The MIPS assembler has different syntax for .set. We set it to
-   .dummy to trap any errors.  */
-#undef SET_ASM_OP
-#define SET_ASM_OP "\t.dummy\t"
-
-#undef ASM_OUTPUT_DEF
-#define ASM_OUTPUT_DEF(FILE,LABEL1,LABEL2)				\
- do {									\
-	fputc ( '\t', FILE);						\
-	assemble_name (FILE, LABEL1);					\
-	fputs ( " = ", FILE);						\
-	assemble_name (FILE, LABEL2);					\
-	fputc ( '\n', FILE);						\
- } while (0)
-
-#undef ASM_DECLARE_FUNCTION_NAME
-#define ASM_DECLARE_FUNCTION_NAME(STREAM, NAME, DECL)			\
-  do {									\
-    if (!flag_inhibit_size_directive)					\
-      {									\
-	fputs ("\t.ent\t", STREAM);					\
-	assemble_name (STREAM, NAME);					\
-	putc ('\n', STREAM);						\
-      }									\
-    ASM_OUTPUT_TYPE_DIRECTIVE (STREAM, NAME, "function");		\
-    assemble_name (STREAM, NAME);					\
-    fputs (":\n", STREAM);						\
-  } while (0)
-
-#undef ASM_DECLARE_FUNCTION_SIZE
-#define ASM_DECLARE_FUNCTION_SIZE(STREAM, NAME, DECL)			\
-  do {									\
-    if (!flag_inhibit_size_directive)					\
-      {									\
-	fputs ("\t.end\t", STREAM);					\
-	assemble_name (STREAM, NAME);					\
-	putc ('\n', STREAM);						\
-      }									\
-  } while (0)
-
 
