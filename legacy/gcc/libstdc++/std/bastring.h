8b70c20ed25f905e7e217376c9e2f65a8dee6729
Adrien Destugues <pulkomandy@gmail.com>
2013-01-05 11:19:40 +0100

    Add another standard C++ prototype for string::compare.

--- legacy/gcc/libstdc++/std/bastring.h
+++ legacy/gcc/libstdc++/std/bastring.h
@@ -408,17 +408,18 @@ public:
   basic_string substr (size_type pos = 0, size_type n = npos) const
     { return basic_string (*this, pos, n); }
 
-  int compare (const basic_string& str, size_type pos = 0, size_type n = npos) const;
-  // There is no 'strncmp' equivalent for charT pointers.
-
   // BeOS bogus version
   int compare (const charT* s, size_type pos, size_type n) const;
+  int compare (const basic_string& str, size_type pos = 0, size_type n = npos) const;
+  // There is no 'strncmp' equivalent for charT pointers.
 
   // Correct std C++ prototype
   int compare (size_type pos, size_type n, const charT* s) const
     { return compare(s, pos, n); }
   int compare (const charT* s, size_type pos = 0) const
     { return compare (s, pos, traits::length (s)); }
+  int compare (size_type pos, size_type n, const basic_string& str) const
+	{ return compare(str, pos, n); }
 
   iterator begin () { selfish (); return &(*this)[0]; }
   iterator end () { selfish (); return &(*this)[length ()]; }
